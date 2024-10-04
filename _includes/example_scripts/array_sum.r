1ae5601fbfa886294c56eed83384f97e1ae6a43c
Callum Walley <callum.walley@nesi.org.nz>
2024-10-04 15:32:15 +1300

    Removed uneeded second  R Script

--- _includes/example_scripts/array_sum.r
+++ _includes/example_scripts/array_sum.r
@@ -1,19 +1,54 @@
-#!/usr/bin/env {{ site.example.shell }} 
+#!/usr/bin/env Rscript
 
-library(doParallel)
+suppressPackageStartupMessages(library(doParallel))
 
-num_cpus <- 2 # Use this value to request num cpus.
-size_array <- 20000 
+# Function for shared memory execution
+doTask <- function(size_x, size_y, seed, print_progress){
+    message(sprintf("Summing [ %e x %e ] matrix, seed = '%i'",size_x,size_y, seed))
+    message(sprintf("Running on '%s' with %i CPU(s).", Sys.info()["nodename"], num_cpus))
 
-registerDoParallel(num_cpus)    
+    set.seed(seed)
 
-sprintf("Using %i cpus to sum [ %e x %e ] matrix.",num_cpus,size_array,size_array)
+    registerDoParallel(num_cpus)
 
-results <- foreach(z=0:size_array) %dopar% {
-    percent_complete= z*100/size_array
-    if (percent_complete%%1==0){
-        cat(sprintf(" %i%% done...\r", percent_complete))
+    results_all <- foreach(z=0:size_x) %dopar% {
+        percent_complete= z*100/size_x
+        if (print_progress && percent_complete%%1==0){
+            message(sprintf(" %i%% done...\r", percent_complete))
+        }
+        sum(rnorm(size_y))
     }
-    sum(rnorm(size_array))
+    Reduce("+",results_all)
 }
-sprintf("Sums to %f",  Reduce("+",results))
+
+# 50 calculations, store the result in 'x'
+
+ntasks <- strtoi(Sys.getenv('SLURM_NTASKS', unset = "1")) 
+seed <- strtoi(Sys.getenv('SLURM_ARRAY_TASK_ID', unset = "0"))
+num_cpus <- strtoi(Sys.getenv('SLURM_CPUS_PER_TASK', unset = "1"))
+
+size_x <-60000 # This on makes memorier
+size_y <-40000 # This one to make longer
+
+# Time = (size_x/n) * size_y + c
+# Mem  = (size_x * n) * c1 + size_y * c2
+
+print_progress <- TRUE
+# print_progress <- interactive() # Whether to print progress or not.
+
+#If more than 1 task, use doMPI 
+if (ntasks > 1){
+    suppressPackageStartupMessages(library(doSNOW))
+     cl <- makeSOCKcluster(outfile="", 2)
+
+    results_all <- foreach(z=1:ntasks) %dopar% {
+        doTask(size_x, ceiling(size_y/ntasks), z+seed, print_progress)
+    }
+    
+    results = Reduce("+",results_all)
+    stopCluster(cl)
+    message(sprintf("Sums to %f", results))
+}else{
+    results = doTask(size_x, size_y, seed, print_progress)
+    message(sprintf("Sums to %f", results))
+}
